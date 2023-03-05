631d75222d40b21dced835b85dacfd1d04cd8c1b
HenarMD <henarmd.8.2@gmail.com>
2023-03-05 11:32:27 +0100

    correción nivel categoryName

--- model/level.js
+++ model/level.js
@@ -43,7 +43,7 @@ var categoryblocksinfoSchema = new Schema({
 });
 
 var categorydataSchema = new Schema({
-    categoryName: {type:String, required: true},
+    categoryName: type:String,
     blocksInfo: [categoryblocksinfoSchema]
 });
 var activeblocks = new Schema({
